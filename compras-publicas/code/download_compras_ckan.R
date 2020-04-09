515803f6a052a186e71df6480fa205fa6ef031be
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2020-04-09 11:20:25 -0300

    Separa script compras_ckan.R em download e conferencia

--- compras-publicas/code/download_compras_ckan.R
+++ compras-publicas/code/download_compras_ckan.R
@@ -1,4 +1,4 @@
-library(tidyverse); library(data.table); library(ckanr)
+library(tidyverse); library(ckanr)
 
 path <- "compras-publicas/data-raw/ckan/"
 
@@ -15,15 +15,3 @@ pkg$resources %>%
   select(url) %>% 
   mutate(destfile = file.path(path, basename(url))) %>% 
   pwalk(~safe_download_file(url = .x, destfile = .y))
-
-#======================================================================
-# determinar quais arquivos nao estao disponiveis no CKAN
-
-files <- list.files(path, pattern = "[.]csv$", full.names = FALSE)
-
-files %>% 
-  str_remove_all("[.]csv") %>% 
-  str_split("(?=\\d{4})", simplify = TRUE) %>% 
-  as.data.frame() %>% 
-  reshape2::dcast(V1 ~ V2, fun.aggregate = length, value.var = "V2") %>% 
-  filter_all(any_vars(. == 0))

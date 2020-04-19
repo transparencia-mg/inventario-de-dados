e16155013765e89dd8d4028f08b8f840f6a199f0
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2020-04-19 10:14:13 -0300

    Refatora script para definicao inicial de variaveis

--- obitos-covid19/code/script.R
+++ obitos-covid19/code/script.R
@@ -8,9 +8,13 @@ cols_spec <- c(
   "Data do óbito",
   "Comorbidade")
 
-rng <- "A3:F42"
+RNG <- "A3:F42"
+INPUT <- "obitos-covid19/data-raw/OBITOS_CONF_COVID-19_MG_19.04.2020.xlsx"
+URL <- "http://www.transparencia.dadosabertos.mg.gov.br/dataset/d869e025-9a83-4c64-8d3a-20238786279a/resource/449ca37f-4678-4c8e-8851-afcfaea940df/download/obitosconfcovid19mg20200418.csv"
+N <- 39
+OUTPUT <- "obitos-covid19/data/obitosconfcovid19mg20200419.csv"
 
-dt_raw <- read_excel("obitos-covid19/data-raw/OBITOS_CONF_COVID-19_MG_18.04.2020.xlsx", range = rng)
+dt_raw <- read_excel(INPUT, range = RNG)
 
 infer_spec <- lapply(dt_raw, class)
 
@@ -19,17 +23,18 @@ stopifnot(all(names(infer_spec) == cols_spec))
 dt <- dt_raw %>% 
       mutate(`Data do óbito` = as.numeric(format(`Data do óbito`, "%Y%m%d")))
 
-
-previous <- read_csv2("http://www.transparencia.dadosabertos.mg.gov.br/dataset/d869e025-9a83-4c64-8d3a-20238786279a/resource/9e370f13-125f-4ee8-94a2-4f54d3ffa776/download/obitosconfcovid19mg20200417.csv",
-                      locale = locale(encoding = "latin1"))
+previous <- read_csv2(URL, 
+                      locale = locale(encoding = "UTF-8", decimal_mark = ",", grouping_mark = "."))
 
 
 check <- cols_spec %>% 
-         map(~ dt[[.x]][1:35] == previous[[.x]]) %>% 
+         map(~ dt[[.x]][1:N] == previous[[.x]]) %>% 
          set_names(cols_spec)
 
 check %>% 
-  map(all)
+  map(all) %>% 
+  unlist() %>% 
+  all() %>% 
+  stopifnot()
 
-
-write_excel_csv2(dt, "obitos-covid19/data/obitosconfcovid19mg20200418.csv")
+write_excel_csv2(dt, OUTPUT)
